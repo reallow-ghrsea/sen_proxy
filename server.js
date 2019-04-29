a1d84f61e499ce8b79f7634f35e1a4019ec3fe86
senwLu <swei.lu1018@gmail.com>
2019-04-29 16:34:37 -0700

    connected proxy to component

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const axios = require('axios');
+const cors = require('cors');
+
+const servicePath = 'http:localhost:8081';
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(morgan('dev'));
+
+console.log(__dirname);
+
+app.use('/:houseId', express.static(path.resolve(__dirname, 'public')));
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get("/houseId", cors(), (req, res) => {
+  // let houseId = req.params.houseId;
+  axios
+    .get(`http://localhost:8081/houseId/properties/${req.params.houseId}`)
+    .then(data => {
+      res.send(data.data);
+    })
+    .catch(e => {
+      console.log(e);
+    });
+});
+
+// app.get("/houseId/premierAgents/:houseId/:id", cors(), (req, res) => {
+//   let houseId = req.params.houseId + req.params.id;
+//   axios
+//     .get(`http://localhost:8081/houseId/listedAgent/${houseId}`)
+//     .then(data => {
+//       res.send(data.data);
+//     })
+//     .catch(e => {
+//       console.log(e);
+//     });
+// });
+
+
+
+app.listen(port, () => {
+  console.log(`Proxy Server running at port ${port}`)
+});
