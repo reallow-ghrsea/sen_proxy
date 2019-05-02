88ea02b17c541d63d80234d363316d94fcbfa9a3
senwLu <swei.lu1018@gmail.com>
2019-05-01 21:40:45 -0700

    successfully connect proxy to component on localhost

--- server/index.js
+++ server/index.js
@@ -5,13 +5,8 @@ const cors = require('cors');
 require('newrelic');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-
-
-console.log(__dirname);
-
-app.use('/:houseId', express.static(path.resolve(__dirname, 'public')));
+app.use(express.static(__dirname + '/../public/'));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -19,10 +14,11 @@ app.use(function (req, res, next) {
   next();
 });
 
-app.get("/houseId", cors(), (req, res) => {
-  // let houseId = req.params.houseId;
+app.get("/houseId/*", cors(), (req, res) => {
+  console.log(req.params.houseId)
   axios
-    .get(`http://localhost:8081/houseId/properties/${req.params.houseId}`)
+    .get(`http://ec2-54-244-76-221.us-west-2.compute.amazonaws.com:8081${req.originalUrl}`)
+
     .then(data => {
       res.send(data.data);
     })
@@ -31,6 +27,9 @@ app.get("/houseId", cors(), (req, res) => {
     });
 });
 
+app.use('*', express.static(__dirname + '/../public/index.html'));
+
+const port = process.env.PORT || 9000;
 
 app.listen(port, () => {
   console.log(`Proxy Server running at port ${port}`)
